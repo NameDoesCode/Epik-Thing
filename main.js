50a9c5e90239e13fc9053b1c6f630dd0bec8214d
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-07-18 17:56:10 -0400

    Added eval with readline

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
 var Discord = require("discord.js"), ctx = new Discord.Client(), fs = require('fs'), vm = require('vm'), request = require("request"), cp = require('child_process'), botses;
 String.prototype.json = function() {
- return JSON.parse(fs.readFileSync(this.toString() + '.json').toString());
-				};
-var readline = require('readline');
-var rl = readline.createInterface({"prompt":"Eval>"});
+ return JSON.parse(fs.readFileSync(this.toString() + '.json').toString())
+				}
+var readline = require('readline'), process = require("process"), rl = readline.createInterface({input: process.stdin,output: process.stdout, prompt: "Eval>"});
 rl.prompt();
-rl.on("line",(l)=>{
-eval("console.log('"+l+"')");
-});
+rl.on("line",(l)=>{eval("console.log('"+l+"')");});
 ctx.on('message', message => {
-	var arg = message.content.split(' '), cmd = arg[0].toLowerCase();
+	var arg = message.content.split(' '), cmd = arg[0].toLowerCase()
 	if(cmd == "~stop"&&message.author.id=="450026451115311125") {
 		if(botses != undefined){
-			message.channel.send("Destroying the client.").then((m)=>{em=m});
-			botses.kill("SIGINT");
+			message.channel.send("Destroying the client.").then((m)=>{em=m})
+			botses.kill("SIGINT")
 			botses = undefined;
-		}else message.channel.send("The session is already down.");
+		}else message.channel.send("The session is already down.")
 		}
 	if(cmd == "~start"&&message.author.id=="450026451115311125") {
 		if(botses == undefined){
-			init();
-			message.channel.send("Connecting the client.");
-		}else message.channel.send("The session is already up.");
+			init()
+			message.channel.send("Connecting the client.")
+		}else message.channel.send("The session is already up.")
 		}
 	if(cmd == "~reload"&&message.author.id=="450026451115311125") {
-		var em = undefined;
-		message.channel.send("Destroying the client.").then((m)=>{em=m});
-			botses.kill("SIGINT");
+		var em
+		message.channel.send("Destroying the client.").then((m)=>{em=m})
+			botses.kill("SIGINT")
 			botses = undefined;
-			init();
-			message.channel.send("Connecting the client.");
+			init()
+			message.channel.send("Connecting the client.")
 }
 	
 		})
 function init() {
-			botses = cp.fork('./bot.js');
+			botses = cp.fork('./bot.js')
 			}
-init();
-Bot = './configuration'.json().Bot;
-ctx.login(Bot.token);
+			//cp.exec('sh ./load.sh', (error, stdout, stderr) => { if (error) { console.error(`exec error: ${error}`); return; } console.log(stdout)});
+init()
+Bot = './configuration'.json().Bot
+ctx.login(Bot.token)
