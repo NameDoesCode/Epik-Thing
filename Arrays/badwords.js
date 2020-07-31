9f8f15db1992c39a91b4daf3c039211b2bb73bb6
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-07-31 19:51:39 -0400

    Ima just be adding bad words here. The plan is to replace the characters like "****" instead of the f word.

--- Arrays/badwords.js
+++ Arrays/badwords.js
@@ -1,1 +1,1 @@
-["nigger","niger","nigga","negro","cp","childporn","porn","cracker","chink","jew"]
+["nigger","niger","nigga","negro","cp","childporn","porn","cracker","chink","jew","cazzo","dick","suck my","cum","lick my","lick mia","suck mia","frocio","fag","fagg","faggot","whore","slut","child porn","cock","puttana","pussy","puss","slut","rape","force"]
