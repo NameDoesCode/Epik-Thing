b799e8db03d7f6003ebab246f6d5339f625dddb3
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-07-31 18:57:02 -0400

    Here aswell ffs

--- Modules/Moderation/kick.js
+++ Modules/Moderation/kick.js
@@ -1,9 +1,9 @@
 var i = true
-if(message.mentions.members.first().highestRole.position <= message.member.highestRole.positon || message.mentions.members.first().user.id == message.guild.owner.id) {
+if(message.mentions.members.first().highestRole.position >= message.member.highestRole.positon || message.mentions.members.first().user.id == message.guild.owner.id) {
 	message.chanel.send("They're your superior silly.")
 	i = false
 	}
-if(i && message.mentions.members.first().highestRole.position <= message.guild.me.highestRole.position) {
+if(i && message.mentions.members.first().highestRole.position >= message.guild.me.highestRole.position) {
 	message.chanel.send(`*Try giving Sentire a role that's above **${message.mentions.members.first().highestRole.name}** and try again.*`)
 	i = false
 	}
