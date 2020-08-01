a7e57b613837d05ebde87430962fc256facea830
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-08-01 14:13:24 -0400

    Revamp?

--- Modules/Tools/nick.js
+++ Modules/Tools/nick.js
@@ -1,4 +1,11 @@
- message.mentions.members.first().setNickname(message.content.replace(arg[0] + " ","").replace(message.mentions.users.first(),"")).then((user)=>{
-	message.channel.send(`*Set **${message.mentions.users.first().tag}** nickname to **${message.content.replace(arg[0]+" ","").replace(message.mentions.users.first(),"")}** I hope they enjoy your choice.*`)
-	})
+var cont = message.content.replace(arg[0] + " ","").replace(message.mentions.users.first(),""), before = message.mentions.members.first().displayName
+message.mentions.members.first().setNickname(cont)
+    var embd = new Discord.RichEmbed()
+    embd.setTitle("Nickname Changed.")
+    embd.addField("Executer",message.author.tag)
+    embd.addField("Target",message.mentions.users.first().tag)
+    if(cont == " ") cont = message.mentions.users.first().username
+    embd.addField("New",cont)
+    if(message.mentions.users.first().username != before) embd.addField("Before",before)
+	message.channel.send(embd)
 	 
