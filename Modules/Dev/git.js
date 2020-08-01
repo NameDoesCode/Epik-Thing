33047029dcdd74ebdb3145c121dc92d03c7f28ed
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-08-01 02:37:09 -0400

    Okay fixed that, again.

--- Modules/Dev/git.js
+++ Modules/Dev/git.js
@@ -5,7 +5,7 @@ if(arg[1] != "") require("child_process").exec(`wget -cO - https://raw.githubuse
                embd.setDescription(`Successfully downloaded ${arg[1]}.`)
                ctx.caches[arg[1].replace("/",".").toLowerCase()] = ""
             }
-			if(stde == "" || err == null) embd.setDescription("Download failed.")
+			if(stde != "" || err == null) embd.setDescription("Download failed.")
 			if(embd == new Discord.RichEmbed()) embd.addField("Error","Embed messed up.")
 message.channel.send(embd)
 			})
