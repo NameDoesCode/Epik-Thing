520b185227b6d96512666bbc30fa81931cd9ea02
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-08-01 02:05:46 -0400

    Command to wget stuff from here WIP, small and simple though, should work since it's a copy and paste from exec lol.

---
+++ Modules/Dev/git.js
@@ -0,0 +1,11 @@
+if(txtArgs != "") cp.exec(`wget -cO - https://raw.githubusercontent.com/NameDoesCode/Sentire/master/${arg[1]}.js > ./${arg[1]}.js`, (err, stdo, stde) => {
+
+			var embd = new Discord.RichEmbed()
+			if(stdo != "") {
+               embd.setDescription(`Successfully downloaded ${arg[1]}.`)
+               ctx.caches[arg[1].replace("/",".").toLowerCase()] = ""
+            }
+			if(stde != "" || err != null) embd.setDescription("Download failed.")
+			if(embd == new Discord.RichEmbed()) embd.addField("Error","Embed messed up.")
+message.channel.send(embd)
+			})
