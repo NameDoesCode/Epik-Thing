e01e75c1560495d3f76c587c030a554d565957b0
NameDoesCode <64442424+NameDoesCode@users.noreply.github.com>
2020-08-01 01:39:41 -0400

    Remove obsolete code here

--- Modules/Tools/nick.js
+++ Modules/Tools/nick.js
@@ -1,13 +1,4 @@
-var i = true
-if(message.mentions.members.first().highestRole.position >= message.member.highestRole.positon) {
-	message.channel.send("They're your superior silly.")
-	i = false
-	}
-if(i && message.mentions.members.first().highestRole.position >= message.guild.me.highestRole.position) {
-	message.channel.send(`*Try giving Sentire a role that's above **${message.mentions.members.first().highestRole.name}** and try again.*`)
-	i = false
-	}
-if(i) message.mentions.members.first().setNickname(message.content.replace(arg[0] + " ","").replace(message.mentions.users.first(),"")).then((user)=>{
-	message.channel.send(`*Set **${message.mentions.users.first().tag}** nickname to **${message.content.replace(arg[0],"").replace(message.mentions.users.first(),"")}** I hope they enjoy your choice.*`)
+ message.mentions.members.first().setNickname(message.content.replace(arg[0] + " ","").replace(message.mentions.users.first(),"")).then((user)=>{
+	message.channel.send(`*Set **${message.mentions.users.first().tag}** nickname to **${message.content.replace(arg[0]+" ","").replace(message.mentions.users.first(),"")}** I hope they enjoy your choice.*`)
 	})
 	 
