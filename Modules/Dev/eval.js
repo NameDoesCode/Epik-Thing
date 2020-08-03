0bde508c284eda7bf0135ea39b985d41c266464d
Michael <64442424+NameDoesCode@users.noreply.github.com>
2020-08-03 05:16:06 -0400

    Added eval

---
+++ Modules/Dev/eval.js
@@ -0,0 +1,9 @@
+String.prototype.json = function() {
+				 return JSON.parse(fs.readFileSync(this.toString() + '.json').toString())
+				}
+if('./Arrays/owners'.json().hasOwnProperty(message.author.id.toString())) {
+	const os = require("os")
+	try{
+	if(message.content.replace(arg[0] + ' ', '') != undefined) eval(message.content.replace(arg[0] + ' ', ''))
+	}catch(err){message.channel.send(`\`\`\`${err.toString()}\`\`\``)}
+	}
